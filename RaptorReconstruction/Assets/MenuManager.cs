b07530b7755937ba525426fb0fe2e3a5e36e9439
Unknown <jakeshmearlof4@gmail.com>
2018-09-18 01:59:59 -0600

    Finished the UI Prototype

---
+++ RaptorReconstruction/Assets/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+
+    public GameObject RightRadialPanel;
+    public GameObject LeftRadialPanel;
+    public GameObject FactsMenu;
+    public GameObject LayersMenu;
+    public GameObject PauseMenu;
+
+    public void RIghtRadialMenuToggle()
+    {
+        if (RightRadialPanel.activeSelf || LeftRadialPanel.activeSelf)
+        {
+            RightRadialPanel.SetActive(false);
+            LeftRadialPanel.SetActive(false);
+        }
+        else
+        {
+            RightRadialPanel.SetActive(true);
+        }
+    }
+
+    public void LeftRadialMenuToggle()
+    {
+        if (RightRadialPanel.activeSelf || LeftRadialPanel.activeSelf)
+        {
+            RightRadialPanel.SetActive(false);
+            LeftRadialPanel.SetActive(false);
+        }
+        else
+        {
+            LeftRadialPanel.SetActive(true);
+        }
+    }
+
+
+}
---
+++ RaptorReconstruction/Assets/Scripts/TextStrings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextStrings : MonoBehaviour {
+
+
+
+    public string FirstDiscoveryTXT,ExcavationTXT, StateDinoTXT, WhatsHappeningText;
+    public GameObject thisContentBox;
+
+    private void Start()
+    {
+        FirstDiscoveryTXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        ExcavationTXT = "Dui sapien eget mi proin. Duis ut diam quam nulla porttitor massa id neque aliquam. Tellus molestie nunc non blandit massa enim nec dui nunc. Senectus et netus et malesuada. Molestie at elementum eu facilisis sed odio morbi. Dolor sit amet consectetur adipiscing. Etiam sit amet nisl purus in mollis nunc. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Elementum eu facilisis sed odio morbi quis commodo. Aliquam ultrices sagittis orci a scelerisque purus. Nunc eget lorem dolor sed viverra ipsum.";
+        StateDinoTXT = "Nulla pellentesque dignissim enim sit amet venenatis urna cursus eget. Eros donec ac odio tempor orci. Feugiat sed lectus vestibulum mattis ullamcorper velit. Felis eget velit aliquet sagittis id consectetur purus ut faucibus. Euismod nisi porta lorem mollis aliquam ut porttitor leo. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit";
+        WhatsHappeningText = "Euismod lacinia at quis risus sed vulputate odio. Nisl vel pretium lectus quam id leo in vitae turpis. Massa tempor nec feugiat nisl pretium fusce id velit. Est sit amet facilisis magna etiam tempor. Nibh tellus molestie nunc non blandit massa enim. Vulputate eu scelerisque felis imperdiet. Aenean sed adipiscing diam donec. Purus in massa tempor nec feugiat nisl ";
+    }
+
+
+
+
+}
