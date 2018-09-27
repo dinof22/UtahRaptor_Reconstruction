b71d20d0419091afa82bd155ddfbb334f6bb7a54
Unknown <jakeshmearlof4@gmail.com>
2018-09-27 02:34:37 -0600

    added bens level, more ui changes, radial menus still not done

--- RaptorReconstruction/Assets/MenuManager.cs
+++ RaptorReconstruction/Assets/MenuManager.cs
@@ -12,6 +12,7 @@ public class MenuManager : MonoBehaviour {
     public GameObject PauseMenu;
     public GameObject LeftToolTips;
     public GameObject RIghtToolTips;
+    public GameObject LeftJakesRadial, RightJakesRadial;
 
     public void RIghtRadialMenuToggle()
     {
@@ -19,12 +20,19 @@ public class MenuManager : MonoBehaviour {
         {
             RightRadialPanel.SetActive(false);
             LeftRadialPanel.SetActive(false);
+            RightJakesRadial.SetActive(false);
+            LeftJakesRadial.SetActive(false);
+
             RIghtToolTips.SetActive(true);
             LeftToolTips.SetActive(true);
         }
         else
         {
             RightRadialPanel.SetActive(true);
+
+            RightJakesRadial.SetActive(true);
+            LeftJakesRadial.SetActive(true);
+
             RIghtToolTips.SetActive(false);  //tooltips off when radialmenu is active
             LeftToolTips.SetActive(false);
 
@@ -37,12 +45,19 @@ public class MenuManager : MonoBehaviour {
         {
             RightRadialPanel.SetActive(false);
             LeftRadialPanel.SetActive(false);
+            RightJakesRadial.SetActive(false);
+            LeftJakesRadial.SetActive(false);
+
             RIghtToolTips.SetActive(true);
             LeftToolTips.SetActive(true);
         }
         else
         {
             LeftRadialPanel.SetActive(true);
+
+            RightJakesRadial.SetActive(true);
+            LeftJakesRadial.SetActive(true);
+
             RIghtToolTips.SetActive(false);
             LeftToolTips.SetActive(false);
         }
