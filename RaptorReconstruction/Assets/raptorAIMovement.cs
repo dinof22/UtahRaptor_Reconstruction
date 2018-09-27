cc969034abc76d2c9f0d7449f1e7baacecc0549a
Unknown <jakeshmearlof4@gmail.com>
2018-09-27 13:59:31 -0600

    baked lighting? and added unity's navmesh components. so many files!

---
+++ RaptorReconstruction/Assets/raptorAIMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class raptorAIMovement : MonoBehaviour {
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        if (there are any objetcts with a specific tag)
+        {
+            setDestination of closest object
+                if (if the distance of selected object is less than 2 feet)
+            {
+                Destroy selected object
+            }
+        }
+        */
+	}
+}
