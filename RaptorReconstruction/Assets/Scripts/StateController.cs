7000d31d9cf82a8deca29bcf1164edbc7656adc2
Unknown <jakeshmearlof4@gmail.com>
2018-10-09 15:14:52 -0600

    made waypoints

---
+++ RaptorReconstruction/Assets/Scripts/StateController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class StateController : MonoBehaviour {
+
+    [HideInInspector] public NavMeshAgent _navMeshAgent;
+    [HideInInspector] public List<Transform> wayPointList;
+}
