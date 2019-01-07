dae6fda30430c5a31eb7f5d1d1131bf5efa627f6
Unknown <jakeshmearlof4@gmail.com>
2019-01-07 10:40:46 -0700

    added the rock slab obj

--- RaptorReconstruction/Assets/RaptorAI.cs
+++ RaptorReconstruction/Assets/RaptorAI.cs
@@ -18,6 +18,7 @@ public class RaptorAI : MonoBehaviour {
     public float drink_level;
     public float scratch_level;
     public float curiosity_level;
+    public float idle_level;
 
     public List<Transform> wayPointList;
     public bool alive = true;
@@ -37,7 +38,10 @@ public class RaptorAI : MonoBehaviour {
         Idle,
         Eat,
         Drink,
-        Scratch
+        Scratch,
+        summon,
+        lookAtPlayer
+
     };
 
 
@@ -55,6 +59,7 @@ public class RaptorAI : MonoBehaviour {
         drink_level = UnityEngine.Random.Range(0, 100);
         scratch_level = UnityEngine.Random.Range(0, 100);
         curiosity_level = UnityEngine.Random.Range(0, 100);
+        idle_level = 100;
     }
 
     private void Update()
@@ -67,6 +72,8 @@ public class RaptorAI : MonoBehaviour {
         food_level -= 1 * Time.deltaTime;
         drink_level -= 1 * Time.deltaTime;
         scratch_level -= 1 * Time.deltaTime;
+        idle_level -= 1.5f * Time.deltaTime;
+        curiosity_level -= 1.2f * Time.deltaTime;
 
 
     }
@@ -95,6 +102,12 @@ public class RaptorAI : MonoBehaviour {
                     case states.Scratch:
                         StartCoroutine("scratch_Action");
                         break;
+                    case states.Idle:
+                        print("path complete check hitting idle");
+                        break;
+                    case states.lookAtPlayer:
+                        StartCoroutine("lookAtPlayer_Action");
+                        break;
                     default:
                         break;
                 }
@@ -110,27 +123,43 @@ public class RaptorAI : MonoBehaviour {
     //finds lowest need value need to see if I can change to a switch statement
     public void Find_lowest_stat()
     {
-        if (Mathf.Min(food_level, drink_level, scratch_level) == food_level)
+        //temporary variable created that is minimum for all the stat levels
+        float minimumStat = Mathf.Min(food_level, drink_level, scratch_level, idle_level, curiosity_level);
+
+        if (minimumStat == food_level)
         {
             print("Im hungry");
             current_State = states.Eat;
             Eat_State();
         }
-        else if (Mathf.Min(food_level, drink_level, scratch_level) == drink_level)
+        else if (minimumStat == drink_level)
         {
             print("Im thirsty");
             current_State = states.Drink;
             Drink_State();
         }
-        else if (Mathf.Min(food_level, drink_level, scratch_level) == scratch_level)
+        else if (minimumStat == scratch_level)
         {
             print("I gotta scratch something");
             current_State = states.Scratch;
             Scratch_State();
         }
+        else if (minimumStat == idle_level)
+        {
+            print("I want to idle");
+            current_State = states.Idle;
+            Idle_state();
+        }
+        else if (minimumStat == curiosity_level)
+        {
+            print("I want to look at the player");
+            current_State = states.lookAtPlayer;
+            lookAtPlayer_state();
+
+        }
         else
         {
-            print(Mathf.Min(food_level,drink_level,scratch_level));
+            print(minimumStat);
             print("couldnt get the lowest stat to match anything");
             current_State = states.Idle;
             Idle_state();
@@ -139,7 +168,7 @@ public class RaptorAI : MonoBehaviour {
 
 
 
-
+    /*
     IEnumerator AI_Update()
     {
         while (alive)
@@ -160,25 +189,43 @@ public class RaptorAI : MonoBehaviour {
         }
         yield return new WaitForEndOfFrame();
     }
-
+    */
 
     public void Idle_state()
     {
         if (current_State == states.Idle)
         {
             print("im idling");
+            StartCoroutine("idle_Action");
+            print("destination is: " + wayPointList[1].position);
         }
+    }
+    IEnumerator idle_Action()
+    {
+        print("running idle coroutine");
+        idle_level = 100;
+        yield return new WaitForSeconds(5f);
+        Find_lowest_stat();
     }
 
 
+    public void lookAtPlayer_state()
+    {
+        print("im going to look at the player");
+        raptorAgent.stoppingDistance = 5;
+        raptorAgent.SetDestination(player_Position.position);
+    }
+    IEnumerator lookAtPlayer_Action()
+    {
+        curiosity_level = 100;
+        print("im looking at the player now");
+        yield return new WaitForSeconds(5f);
+        print("this guy is boring");
+        raptorAgent.stoppingDistance = 2;
+        Find_lowest_stat();
+    }
+
     //keep the states here in the order the transforms are set in the waypoint list (for simplicity)
-
-
-
-
-
-
-
     /// <summary>
     /// ////////////////////////////////////////Scratch State
     /// </summary>
@@ -275,8 +322,15 @@ public class RaptorAI : MonoBehaviour {
     /// </summary>
     public void Summon_State()
     {
-        print("Im going to the player");
+        print("the player has summoned me!");
         raptorAgent.SetDestination(player_Position.position);
+        raptorAgent.stoppingDistance = 5;
 
+    }
+
+    public void dismiss()
+    {
+        Find_lowest_stat();
+        raptorAgent.stoppingDistance = 2;
     }
 }
